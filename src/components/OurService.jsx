89445ce39e6a8710d33d3caa1e3a61df3c2024fd
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-08-10 22:14:54 -0300

    feat: social area adding px at the end of max-width: 1919

--- src/components/OurService.jsx
+++ src/components/OurService.jsx
@@ -1,5 +1,8 @@
 import { AiOutlineSafety, AiOutlineIssuesClose, AiOutlineTool } from "react-icons/ai";
 import styles from '../styles/ourService.module.css';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function OurService() {
   return(
@@ -34,6 +37,38 @@ function OurService() {
         <button className={styles.knowMoreBtn}> Saiba mais </button>
       </div>
     </div>
+    <Carousel>
+    <Carousel.Item interval={null}>
+      <div className={ styles.homeCard }>
+        <AiOutlineSafety fontSize="6em" />
+        <div>
+        <p>Assistência técnica autorizada para produtos dentro de garantia</p>
+        <p>Descubra as marcas e produtos com assistência técnica e com garantia</p>
+        </div>
+        <button className={ styles.knowMoreBtn }> Saiba mais </button>
+      </div>
+      </Carousel.Item>
+      <Carousel.Item interval={null}>
+      <div className={ styles.homeCard }>
+        <AiOutlineIssuesClose fontSize="6em"/>
+        <div>
+          <p> Assistência técnica autorizada para produtos fora de garantia </p>
+          <p> Descubra as marcas e produtos com assistência técnica e com garantia</p>
+        </div>
+        <button className={ styles.knowMoreBtn }> Saiba mais </button>
+      </div>
+      </Carousel.Item>
+      <Carousel.Item interval={null}>
+      <div className={ styles.homeCard }>
+        <AiOutlineTool fontSize="6em"/>
+        <div>
+          <p> Assistência técnica especializada </p>
+          <p> Descubra as marcas e produtos com assistência técnica e com garantia </p>
+        </div>
+        <button className={styles.knowMoreBtn}> Saiba mais </button>
+      </div>
+      </Carousel.Item>
+    </Carousel>
   </section>
   )
 }

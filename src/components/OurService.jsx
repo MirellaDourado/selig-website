ec289fdc457bbcd9e7ed65272a5cc1ad0974656d
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-08-17 17:20:47 -0300

    feat: ourservices style

--- src/components/OurService.jsx
+++ src/components/OurService.jsx
@@ -37,36 +37,36 @@ function OurService() {
         <button className={styles.knowMoreBtn}> Saiba mais </button>
       </div>
     </div>
-    <Carousel>
-    <Carousel.Item interval={null}>
-      <div className={ styles.homeCard }>
-        <AiOutlineSafety fontSize="6em" />
-        <div>
-        <p>Assistência técnica autorizada para produtos dentro de garantia</p>
-        <p>Descubra as marcas e produtos com assistência técnica e com garantia</p>
+    <Carousel className={ styles.ourServiceCarousel } interval={null} style={{ overflow: 'visible'}}>
+      <Carousel.Item style={{ width: '10%', border: '2px red solid'}}>
+        <div className={ styles.homeCard }>
+          <AiOutlineSafety fontSize="6em" />
+          <div>
+          <p>Assistência técnica autorizada para produtos dentro de garantia</p>
+          <p>Descubra as marcas e produtos com assistência técnica e com garantia</p>
+          </div>
+          <button className={ styles.knowMoreBtn }> Saiba mais </button>
         </div>
-        <button className={ styles.knowMoreBtn }> Saiba mais </button>
-      </div>
       </Carousel.Item>
-      <Carousel.Item interval={null}>
-      <div className={ styles.homeCard }>
-        <AiOutlineIssuesClose fontSize="6em"/>
-        <div>
-          <p> Assistência técnica autorizada para produtos fora de garantia </p>
-          <p> Descubra as marcas e produtos com assistência técnica e com garantia</p>
+      <Carousel.Item>
+        <div className={ styles.homeCard }>
+          <AiOutlineIssuesClose fontSize="6em"/>
+          <div>
+            <p> Assistência técnica autorizada para produtos fora de garantia </p>
+            <p> Descubra as marcas e produtos com assistência técnica e com garantia</p>
+          </div>
+          <button className={ styles.knowMoreBtn }> Saiba mais </button>
         </div>
-        <button className={ styles.knowMoreBtn }> Saiba mais </button>
-      </div>
       </Carousel.Item>
-      <Carousel.Item interval={null}>
-      <div className={ styles.homeCard }>
-        <AiOutlineTool fontSize="6em"/>
-        <div>
-          <p> Assistência técnica especializada </p>
-          <p> Descubra as marcas e produtos com assistência técnica e com garantia </p>
+      <Carousel.Item>
+        <div className={ styles.homeCard }>
+          <AiOutlineTool fontSize="6em"/>
+          <div>
+            <p> Assistência técnica especializada </p>
+            <p> Descubra as marcas e produtos com assistência técnica e com garantia </p>
+          </div>
+          <button className={styles.knowMoreBtn}> Saiba mais </button>
         </div>
-        <button className={styles.knowMoreBtn}> Saiba mais </button>
-      </div>
       </Carousel.Item>
     </Carousel>
   </section>

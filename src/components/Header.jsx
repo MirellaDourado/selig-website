070ee770fe33f66777e4d913d4baaad900b0eef1
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-08-18 00:09:18 -0300

    feat: header mobile and desktop done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,37 +4,46 @@ import  { AiOutlineWhatsApp } from "react-icons/ai";
 import logo from '../assets/selig-logo.png';
 import style from '../styles/header.module.css';
 import PropTypes from 'prop-types';
+import { HiMenu } from "react-icons/hi";
+import { useState } from 'react';
 
 function Header({ boxShadow }) {
+  const [ clicked, setClicked ] = useState(false);
+
   return(
-    <>
+    <section className={ style.classMom }>
       <section className={ style.headerSection }>
         <div>
-          <a target="_blank" rel='noopener noreferrer'href='https://www.instagram.com/assistenciaselig/'> FALE CONOSCO <AiOutlineWhatsApp fontSize='1.2em'/> </a>
-          <a target="_blank" rel='noopener noreferrer'href='https://www.instagram.com/assistenciaselig/'> INSTAGRAM <BiLogoInstagramAlt fontSize='1.2em'/> </a>
-          <a target="_blank" rel='noopener noreferrer'href='https://www.tiktok.com/assistenciaselig/'>  TIKTOK <BiLogoTiktok fontSize='1.2em'/> </a>
-          <a target="_blank" rel='noopener noreferrer'href='https://www.linkedin.com/company/assistencia-selig/'> LINKEDIN <BiLogoLinkedin fontSize='1.2em' /> </a>
+          <a target="_blank" rel='noopener noreferrer'href='https://www.instagram.com/assistenciaselig/'> FALE CONOSCO <AiOutlineWhatsApp fontSize='1.2em' className={ style.icon }/> </a>
+          <a target="_blank" rel='noopener noreferrer'href='https://www.instagram.com/assistenciaselig/'> INSTAGRAM <BiLogoInstagramAlt fontSize='1.2em' className={ style.icon }/> </a>
+          <a target="_blank" rel='noopener noreferrer'href='https://www.tiktok.com/assistenciaselig/'>  TIKTOK <BiLogoTiktok fontSize='1.2em' className={ style.icon }/> </a>
+          <a target="_blank" rel='noopener noreferrer'href='https://www.linkedin.com/company/assistencia-selig/'> LINKEDIN <BiLogoLinkedin fontSize='1.2em' className={ style.icon }/> </a>
         </div>
       </section>
       <header className={ style.seligHeader } style={ boxShadow ? { boxShadow } : null }>
-        <nav>
-          {/* <Link to='/'>
-            <img src={ logo } alt='nome SeLiG em laranja' />
-          </Link> */}
-          <ul>
-            <li>
-              <Link to='/selig'> A SeLiG </Link>
-            </li>
-            <li>
-              <a href='/#services'> Nossos Serviços </a>
-            </li>
-            <li>
-              <a href='/#contact'> Contato </a>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <Link to='/' className={ style.logo }>
+            <img src={ logo } alt='nome SeLiG em laranja'/>
+          </Link>
+          <nav id={ style.nav } className={clicked && style.active}>
+            <button onClick={() => setClicked(!clicked)}>
+              <HiMenu className={ style.btnMobile }/>
+            </button>
+            <ul id={ style.menu }>
+              <li>
+                <Link to='/selig'> A SeLiG </Link>
+              </li>
+              <li>
+                <a href='/#services'> Nossos Serviços </a>
+              </li>
+              <li>
+                <a href='/#contact'> Contato </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
-    </>
+    </section>
   )
 }
 

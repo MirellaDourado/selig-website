c7be09bf6e9c0e2f7956d4740c2e4008d47385f2
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-08-24 19:54:55 -0300

    feat: fixing ourService width and margin-top

--- src/pages/Selig.jsx
+++ src/pages/Selig.jsx
@@ -11,7 +11,7 @@ function Selig() {
   return (
     <>
     <Header boxShadow={'rgba(0, 0, 0, 0.24) 0px 3px 8px'} />
-    <main>
+    <main>  
       <a href='https://api.whatsapp.com/send/?phone=557122037800&text&type=phone_number&app_absent=0' className='whatsapp-icon'>
       <AiOutlineWhatsApp />
       </a>

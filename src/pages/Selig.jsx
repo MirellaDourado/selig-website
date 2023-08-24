ce1538c10067cfc650657be321f0a01f5827cba4
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-08-24 19:30:00 -0300

    feat: selig page for mobile

--- src/pages/Selig.jsx
+++ src/pages/Selig.jsx
@@ -15,23 +15,23 @@ function Selig() {
       <a href='https://api.whatsapp.com/send/?phone=557122037800&text&type=phone_number&app_absent=0' className='whatsapp-icon'>
       <AiOutlineWhatsApp />
       </a>
-      <div  style={{ width: '90%', margin: 'auto', marginBottom: '5rem'}}>
+      <div>
         <h2 className='h-config h2-config' style={{ marginTop: '1rem'}}> Sobre Nós </h2>
           <hr className='hr' />
-        <div style={{display: 'flex', gap: '1rem', margin: '2rem', alignItems: 'center'}}>
-        <img src={seligFachada} alt='Estrutura da Selig' width='40%' style={{ borderRadius: '18px'}}/>
+        <div>
+        <img src={seligFachada} alt='Estrutura da Selig' />
         <p>
           A Selig Eletronicos é uma assistência técnica de produtos eletroeletrônicos, fundada em março de 2006, seu objetivo é atender, de maneira diferenciada, os consumidores que precisam dos seus serviços. 
         </p>
         </div>
-        <div style={{display: 'flex', gap: '1rem', margin: '2rem', alignItems: 'center'}}>
+        <div>
           <p>
           Objetivando oferecer maior conforto aos clientes e funcionários, recentemente mudamos de endereço para adequa a nossa estrutura, proporcionando melhores condições de trabalho, além de um aspecto bastante agradável.
           </p>
-          <img src={seligInside} alt='Estrutura interna da Selig' width='40%' style={{ borderRadius: '18px'}}/>
+          <img src={seligInside} alt='Estrutura interna da Selig' />
         </div>
-        <div style={{display: 'flex', gap: '1rem', margin: '2rem', alignItems: 'center'}}>
-          <img width='40%' src={seligBack} style={{ borderRadius: '18px'}}/>
+        <div>
+          <img src={seligBack} alt='Estrutura de armazenamento da selig'/>
           <p>Diferente das outras assistências técnicas, a Selig possui uma estrutura e um atendimento que fazem com que seja escolhida pelos consumidores na hora de confiar seus aparelhos para que seja realizado o reparo.</p>
         </div>
       </div>
